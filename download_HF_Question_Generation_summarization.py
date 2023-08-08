c382938096076c4d4c9625f0e8fc28e521053399
SardarArslan <82857336+SardarArslan@users.noreply.github.com>
2023-08-08 07:53:38 +0500

    Update download_HF_Question_Generation_summarization.py

--- download_HF_Question_Generation_summarization.py
+++ download_HF_Question_Generation_summarization.py
@@ -2,9 +2,9 @@ from transformers.pipelines import pipeline
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 
-tokenizer = AutoTokenizer.from_pretrained("sshleifer/distilbart-cnn-12-6")
-model = AutoModelForSeq2SeqLM.from_pretrained("sshleifer/distilbart-cnn-12-6")
+tokenizer = AutoTokenizer.from_pretrained("Sardar/sql-model-101")
+model = AutoModelForSeq2SeqLM.from_pretrained("Sardar/sql-model-101")
 
 
-model.save_pretrained('./distilbart-cnn-12-6')
-tokenizer.save_pretrained('./distilbart-cnn-12-6')
+model.save_pretrained('./sql')
+tokenizer.save_pretrained('./sql')
